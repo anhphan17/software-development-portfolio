7fbdc234e765c4b2ecab8f373cc497dfffecb82e
anhphan <anhnguyenphan03@gmail.com>
2025-08-25 22:23:57 -0400

    Initial test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProjectDetail from './pages/ProjectDetail';
+import Projects from './pages/Projects';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Layout>
   );
 }
 
