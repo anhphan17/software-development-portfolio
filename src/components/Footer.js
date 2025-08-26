6e2a9ada27e02124f2ca1559ece644f7967c33b2
anhphan <anhnguyenphan03@gmail.com>
2025-08-25 23:57:15 -0400

    Footer added & basic functionality of pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Footer() {
-    return <footer>Footer Works!</footer>;
+    return (
+        <footer style={footerStyle}>
+            <div style={linksStyle}>
+                <a href="https://github.com/anhphan17" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/anhphan17/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            </div>
+        </footer>
+    );
 }
+
+const footerStyle = {
+    backgroundColor: "#222", 
+    color: "#fff",
+    textAlign: "center",
+    padding: "1rem",
+    marginTop: "2rem",
+};
+
+const linksStyle = {
+    display: "flex",
+    justifyContent: "center",
+    gap: "1rem",
+    marginTop: "0.5rem",
+};
 
 export default Footer;
