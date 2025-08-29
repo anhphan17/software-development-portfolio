c8c7470be12c38f1d1f9c93f5243bd0c728b5eab
anhphan <anhnguyenphan03@gmail.com>
2025-08-28 21:03:29 -0400

    Contact page styling update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,11 +2,66 @@ import React from "react";
 
 function Contact() {
     return (
-        <section className="contact" style={{padding: "2rem"}}>
-            <h1>Contact Me</h1>
-            <p>Email: <a href="mailto:anhnguyenphan03@gmail.com">anhnguyenphan03@gmail.com</a></p>
-            <p>LinkedIn: <a href="https://www.linkedin.com/in/anhphan17/">linkedin.com/in/anhphan17/</a></p>
-            <p>GitHub: <a href="https://github.com/anhphan17">github.com/anhphan17</a></p>
+        <section className="contact" 
+            style={{
+            padding: "2rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "1.5rem"
+            }}
+        >
+            <h1 style={{ color: "#001f49" }}>Contact Me</h1>
+
+            <div 
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "0.75rem",
+                    maxWidth: "400px",
+                    width: "100%",
+                    border: "1px solid #ccc",
+                    borderRadius: "10px",
+                    padding: "1rem",
+                    backgroundColor: "#ffffff"
+                }}
+            >
+            
+            <p style={{ textAlign: "center"}}><strong>Email: </strong><a href="mailto:anhnguyenphan03@gmail.com">anhnguyenphan03@gmail.com</a></p>
+            </div>
+
+            <div 
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "0.75rem",
+                    maxWidth: "400px",
+                    width: "100%",
+                    border: "1px solid #ccc",
+                    borderRadius: "10px",
+                    padding: "1rem",
+                    backgroundColor: "#ffffff"
+                }}
+            >
+            <p style={{ textAlign: "center"}}><strong>LinkedIn: </strong><a href="https://www.linkedin.com/in/anhphan17/">linkedin.com/in/anhphan17/</a></p>
+            </div>
+            
+            <div 
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "0.75rem",
+                    maxWidth: "400px",
+                    width: "100%",
+                    border: "1px solid #ccc",
+                    borderRadius: "10px",
+                    padding: "1rem",
+                    backgroundColor: "#ffffff"
+                }}
+            >
+            <p style={{ textAlign: "center"}}><strong>GitHub: </strong><a href="https://github.com/anhphan17">github.com/anhphan17</a></p>
+            </div>
+            
         </section>
         
     );
